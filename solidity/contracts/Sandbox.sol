e2c8de019e66849668eef762bb881f47887a6c61
mrux_ps <mrux@paystand.com>
2022-12-08 14:53:01 -0600

    modules

---
+++ solidity/contracts/Sandbox.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.4 <0.9.0;
+
+import {GnosisSafe} from "@safe-contracts/GnosisSafe.sol";
+
+contract Sandbox {}
--- solidity/scripts/Deploy.sol
+++ solidity/scripts/Deploy.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.4 <0.9.0;
 
-/* import {Script} from 'forge-std/Script.sol';
-import {Greeter} from 'contracts/Greeter.sol';
-import {IERC20} from 'isolmate/interfaces/tokens/IERC20.sol'; */
+import {Script} from "lib/forge-std/src/Script.sol";
+import {Greeter} from "contracts/Greeter.sol";
+
+// import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
 abstract contract Deploy {
 
